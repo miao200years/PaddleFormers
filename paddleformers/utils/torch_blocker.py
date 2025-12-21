23b285595df92d4c28174a8ab1ea5fc9d89ad4e1
miao200years <243062960@qq.com>
2025-12-22 07:10:13 +0800

    fix

--- paddleformers/utils/torch_blocker.py
+++ paddleformers/utils/torch_blocker.py
@@ -30,12 +30,12 @@ class TorchBlocker:
         importlib.util.find_spec = self._fake_find_spec
 
     def _fake_find_spec(self, name, package=None):
-        frame = sys._getframe(1)
-        while frame:
-            filename = frame.f_code.co_filename or ""
-            if "PaddleFormers/tests/" in filename:
-                return self._original_find_spec(name, package)
-            frame = frame.f_back
+        # frame = sys._getframe(1)
+        # while frame:
+        #     filename = frame.f_code.co_filename or ""
+        #     if "PaddleFormers/tests/" in filename:
+        #         return self._original_find_spec(name, package)
+        #     frame = frame.f_back
         if self.block_torch and (name == "torch" or name.startswith("torch.")):
             return None
         return self._original_find_spec(name, package)
@@ -54,12 +54,12 @@ class TorchBlocker:
         return False
 
     def _custom_import(self, name, globals=None, locals=None, fromlist=(), level=0):
-        frame = sys._getframe(1)
-        while frame:
-            filename = frame.f_code.co_filename or ""
-            if "PaddleFormers/tests/" in filename:
-                return self._original_import(name, globals, locals, fromlist, level)
-            frame = frame.f_back
+        # frame = sys._getframe(1)
+        # while frame:
+        #     filename = frame.f_code.co_filename or ""
+        #     if "PaddleFormers/tests/" in filename:
+        #         return self._original_import(name, globals, locals, fromlist, level)
+        #     frame = frame.f_back
         if level > 0 and globals:
             pkg = globals.get("__package__") or globals.get("__name__", "")
             if pkg:
