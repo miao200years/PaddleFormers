abcdd7c62c4f54d26d1e6679401849a10b5e0ccf
miao200years <243062960@qq.com>
2026-01-14 19:08:54 +0800

    fix

--- paddleformers/cli/train/sft/workflow.py
+++ paddleformers/cli/train/sft/workflow.py
@@ -254,19 +254,19 @@ def run_sft(
     model_config.max_sequence_length = data_args.max_seq_len
     model_config._attn_implementation = model_args.attn_impl
 
-    def set_attr_func(config, key, value):
-        if value is not None:
-            setattr(config, key, value)
-
-    set_attr_func(model_config, "num_hidden_layers", model_args.num_hidden_layers)
-    set_attr_func(model_config, "num_attention_heads", model_args.num_attention_heads)
-    set_attr_func(model_config, "num_key_value_heads", model_args.num_key_value_heads)
-    set_attr_func(model_config, "num_experts_per_tok", model_args.num_experts_per_tok)
-    set_attr_func(model_config, "hidden_size", model_args.hidden_size)
-    set_attr_func(model_config, "intermediate_size", model_args.intermediate_size)
-    set_attr_func(model_config, "n_routed_experts", model_args.n_routed_experts)
-    set_attr_func(model_config, "use_qk_norm", model_args.use_qk_norm)
-    set_attr_func(model_config, "tie_word_embeddings", model_args.tie_word_embeddings)
+    # def set_attr_func(config, key, value):
+    #     if value is not None:
+    #         setattr(config, key, value)
+    #
+    # set_attr_func(model_config, "num_hidden_layers", model_args.num_hidden_layers)
+    # set_attr_func(model_config, "num_attention_heads", model_args.num_attention_heads)
+    # set_attr_func(model_config, "num_key_value_heads", model_args.num_key_value_heads)
+    # set_attr_func(model_config, "num_experts_per_tok", model_args.num_experts_per_tok)
+    # set_attr_func(model_config, "hidden_size", model_args.hidden_size)
+    # set_attr_func(model_config, "intermediate_size", model_args.intermediate_size)
+    # set_attr_func(model_config, "n_routed_experts", model_args.n_routed_experts)
+    # set_attr_func(model_config, "use_qk_norm", model_args.use_qk_norm)
+    # set_attr_func(model_config, "tie_word_embeddings", model_args.tie_word_embeddings)
 
     # Sync arguments to MLLM sub_config
     if getattr(model_config, "text_config", None) is not None:
